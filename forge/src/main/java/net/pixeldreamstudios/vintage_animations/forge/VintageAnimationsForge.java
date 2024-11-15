11ca18983f788384a5d4f984e5ea3b4a96453468
Thrasos <100732009+thrasosc@users.noreply.github.com>
2024-11-15 20:23:08 +0100

    change forge client init method

--- forge/src/main/java/net/pixeldreamstudios/vintage_animations/forge/VintageAnimationsForge.java
+++ forge/src/main/java/net/pixeldreamstudios/vintage_animations/forge/VintageAnimationsForge.java
@@ -1,13 +1,19 @@
 package net.pixeldreamstudios.vintage_animations.forge;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.pixeldreamstudios.vintage_animations.VintageAnimations;
 
 @Mod(VintageAnimations.MOD_ID)
 public final class VintageAnimationsForge {
     public VintageAnimationsForge() {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> VintageAnimations::initClient);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::onClientSetup);
+    }
+
+    private void onClientSetup(final FMLClientSetupEvent event) {
+        VintageAnimations.initClient();
     }
 }
