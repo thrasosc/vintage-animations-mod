1940bf7a5cff8423cbfbedd022ee0892ba4fd4f3
Thrasos <100732009+thrasosc@users.noreply.github.com>
2024-11-16 11:49:33 +0100

    use AutoConfig screen for the config screen in Forge

--- forge/src/main/java/net/pixeldreamstudios/vintage_animations/forge/VintageAnimationsForge.java
+++ forge/src/main/java/net/pixeldreamstudios/vintage_animations/forge/VintageAnimationsForge.java
@@ -1,9 +1,13 @@
 package net.pixeldreamstudios.vintage_animations.forge;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pixeldreamstudios.vintage_animations.VintageAnimConfig;
 import net.pixeldreamstudios.vintage_animations.VintageAnimations;
 
 @Mod(VintageAnimations.MOD_ID)
@@ -15,5 +19,6 @@ public final class VintageAnimationsForge {
 
     private void onClientSetup(final FMLClientSetupEvent event) {
         VintageAnimations.initClient();
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> AutoConfig.getConfigScreen(VintageAnimConfig.class, parent).get()));
     }
 }
