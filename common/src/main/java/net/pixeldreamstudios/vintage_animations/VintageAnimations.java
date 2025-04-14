2117dd061ba9c1555b59ae129fa2b8c39b7f28c6
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-14 11:27:40 +0200

    create logger

--- common/src/main/java/net/pixeldreamstudios/vintage_animations/VintageAnimations.java
+++ common/src/main/java/net/pixeldreamstudios/vintage_animations/VintageAnimations.java
@@ -2,9 +2,12 @@ package net.pixeldreamstudios.vintage_animations;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class VintageAnimations {
   public static final String MOD_ID = "vintage_animations";
+  public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
   public static VintageAnimConfig config;
 
   public static void initClient() {
